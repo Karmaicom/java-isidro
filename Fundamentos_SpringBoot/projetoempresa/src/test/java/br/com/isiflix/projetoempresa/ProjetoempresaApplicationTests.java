aee1dc7b8a4635f1359b866b5eae76f8800d07c8
karmaicom <karmaicom@gmail.com>
2025-09-02 21:25:21 -0300

    Commit inicial do projeto empresa

---
+++ Fundamentos_SpringBoot/projetoempresa/src/test/java/br/com/isiflix/projetoempresa/ProjetoempresaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isiflix.projetoempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoempresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
