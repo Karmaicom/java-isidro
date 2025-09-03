16161850efd1918ae156faa03ff71f2c7577e127
karmaicom <karmaicom@gmail.com>
2025-09-03 14:53:58 -0300

    Script insert Departamento

---
+++ SCRIPTS/Insert_Departamento.sql
@@ -0,0 +1,16 @@
+
+
+INSERT INTO `db_empresa`.`departamento` (`numero_id`, `andar`, `nome`)
+VALUES (uuid(), Presidência, 1);
+
+INSERT INTO `db_empresa`.`departamento` (`numero_id`, `andar`, `nome`)
+VALUES (uuid(), Engenharia, 1);
+
+INSERT INTO `db_empresa`.`departamento` (`numero_id`, `andar`, `nome`)
+VALUES (uuid(), Infraestrutura, 2);
+
+INSERT INTO `db_empresa`.`departamento` (`numero_id`, `andar`, `nome`)
+VALUES (uuid(), Telecomunicações, 3);
+
+INSERT INTO `db_empresa`.`departamento` (`numero_id`, `andar`, `nome`)
+VALUES (uuid(), Desenvolvimento, 1);
